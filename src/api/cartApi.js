e72b2bf662d281a7ba998c536f8b86e18d74b9f6
Soju Saji <sojusaji.20@gmail.com>
2026-09-25 20:55:13 +0530

    feat(api): add endpoints to update and remove cart items

--- src/api/cartApi.js
+++ src/api/cartApi.js
@@ -1,12 +1,22 @@
 import api from './axiosInstance';
 
 export const createCart = async (cartData) => {
-    console.log('createCart api is called with cartdata:',cartData);
+    console.log('createCart api is called with cartdata:', cartData);
     const { data } = await api.post('/api/v1/cart', cartData);
     return data;
 }
 
 export const fetchCartData = async () => {
     const { data } = await api.get('/api/v1/cart');
+    return data;
+}
+
+export const updateCartItemQuantity = async () => {
+    const { data } = await api.patch('/api/v1/cart');
+    return data;
+}
+
+export const removeCartItem = async () => {
+    const { data } = await api.delete('/api/v1/cart');
     return data;
 }
