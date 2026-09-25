557dcbdd414ff8d1ecc538d0ed96d70e487342d9
Soju Saji <sojusaji.20@gmail.com>
2026-09-25 20:57:49 +0530

    feat(cart): add custom hooks for updating and removing cart items

--- src/hook/useCart.js
+++ src/hook/useCart.js
@@ -1,6 +1,8 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { createCart, fetchCartData } from "../api/cartApi";
+import { createCart, fetchCartData, updateCartItemQuantity, removeCartItem } from "../api/cartApi";
 import { logError } from "../utils/logger";
+import toast from "react-hot-toast";
+import { cartSchema } from "../../../server/src/utils/authValidator";
 
 
 export const useFetchCart = () => {
@@ -20,7 +22,96 @@ export const useAddToCart = () => {
             queryClient.invalidateQueries({ queryKey: ['cart'] })
         },
         onError: (error) => {
-            logError("Cart update failed:", error);
+            logError("Item adding to cart failed:", error);
+        }
+    })
+}
+
+export const useUpdateCartItemQuantity = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (cartPayload) => updateCartItemQuantity(cartPayload),
+
+        onMutate: async ({ productId, change }) => {
+            await queryClient.cancelQueries({ queryKey: ['cart'] });
+            const previousCart = queryClient.getQueryData(['cart']);
+
+            queryClient.setQueryData(['cart'], (oldData) => {
+                if (!oldData) return oldData;
+                const cartContainer = oldData.cart || oldData;
+                const cartItems = cartContainer.items || [];
+
+                const updatedCartItems = cartItems.map((item) => {
+                    const currentId = item.productId?._id?.toString() || item.productId?.toString();
+                    if (currentId === productId.toString()) {
+                        return {
+                            ...item, quantity: item.quantity + change
+                        }
+                    }
+                    return item;
+                }).filter((item) => item.quantity > 0);
+
+                const newCartData = {
+                    ...cartContainer,
+                    items: updatedCartItems
+                }
+                return oldData.cart ? {
+                    ...oldData, cart: newCartData
+                } : newCartData;
+
+            })
+
+            return { previousCart };
+
+        },
+
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['cart'] })
+        },
+        onError: (error, variables, context) => {
+            if (context?.previousCart) {
+                queryClient.setQueryData(['cart'], (context.previousCart));
+            }
+            logError('Cart item quantity updation failed', error);
+        }
+    })
+}
+
+export const useRemoveCartItem = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (cartPayload) => removeCartItem(cartPayload),
+        onMutate: async ({ productId }) => {
+
+            await queryClient.cancelQueries({ queryKey: ['cart'] });
+            const previousCart = queryClient.getQueryData(['cart']);
+            queryClient.setQueryData(['cart'], (oldData) => {
+                if (!oldData) return oldData;
+
+                const cartContainer = oldData.cart || oldData;
+                const cartItems = cartContainer.items || [];
+                const updatedCartItems = cartItems.filter((item) => {
+                    const currentId = item.productId?._id?.toString() || item.productId?.toString();
+                    return currentId !== productId.toString();
+                });
+                const newCartData = {
+                    ...cartContainer,
+                    items: updatedCartItems
+                };
+                return oldData.cart ? {
+                    ...oldData, cart: newCartData
+                } : newCartData;
+            })
+            return { previousCart };
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['cart'] })
+        },
+        onError: (error, variables, context) => {
+            if (context?.previousCart) {
+                return queryClient.setQueryData(['cart'], (context.previousCart));
+            }
+            logError('Cart item removing failed', error);
         }
     })
 }
